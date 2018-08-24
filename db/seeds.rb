1c3abb84a21217f490454f6211446fc69d223e0a
critsmet <36425627+critsmet@users.noreply.github.com>
2018-08-24 14:26:16 -0400

    Updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,124 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Publication.create(title: "Mask Magazine", tagline: "Culture and style for misfits and malcontents", url: "http://www.maskmagazine.com", cover_image_url: "http://s3.amazonaws.com/issue-assets/authors/images/000/000/131/original/10340159_516349021821649_3881028931068449400_n.png", description: "Mask Magazine is an online repository of youth and internet culture packed with interviews, editorials, news, and style in the age of unrest. Mask is member-supported and advertisement free.", email: "mailroom@mask-mag.com", team: "Chief Editor: Hannah Hurr; Co-Founders: Tyler Reinhard and Ripley Soprano", genres: "Culture & Lifestyle, Society & Capitalism", tags: "New York City, Underground, News, Reviews, Editorials, Interviews, Style, End times")
-Publication.create(title: "Unbag Magazine", tagline: "An arts, culture, and politics publication", url: "http://www.unbag.net" , cover_image_url: "http://www.brooklynartscouncil.org/files/images/31047_Unbag_Square_Icon_Blue-2_1500901207.jpg", description: "Unbag is a semi-annual magazine that promotes critical engagement with contemporary art and politics. Commissioning artists, writers, and thinkers who work outside of mainstream discourses, unbag functions as a space to explore ideas through discussion and exchange.", email: "info@unbag.net", team: "Editors: Aaron Cooper, Charlie Markbreiter, Natalia Tuero; Operations and Production: Andy Wentz; Art Direction and Print Design: American Artist; Web Design and Web Development: Logan Lape", genres: "Art & Design, Culture & Lifestyle, Society & Capitalism", tags: "New York City, Art, Contemporary Art, Politics, Writing, Critique")
-Publication.create(title: "Topical Cream", tagline: "Supporting women and gender-non-confirming individuals in contemporary art", url: "http://www.topicalcream.info/", cover_image_url: "https://i.etsystatic.com/isla/26d5fb/30632791/isla_280x280.30632791_2vwx0fk2.jpg", description: "Topical Cream is a 501(c)3 non-profit covering women, femmes, and gender-nonconforming individuals in contemporary art. Since 2013, the New York-based platform has supported a community of artists, writers, designers, and technologists through digital publishing and public programming initiatives.", email: "rx@topicalcream.info", team: "Co-Founders: Lyndsy Welgos and Ara Anjargolian; Editor: Whitney Mallett", genres: "Culture & Lifestyle, Art & Design, Gender & Sexuality, Science & Technology", tags: "Femme, Fashion, Beauty, Women, Queer, GNC, Art, Contemporary Art, Writing")
-Publication.create(title: "AQNB", tagline: "null", url: "http://www.aqnb.com", cover_image_url: "https://d23pzp3qb0c2ie.cloudfront.net/2018/03/cropped-AQNB-favicon-square-logo.png", description: "AQNB is a not-for-profit editorial platform committed to independent media. Working at the intersection of visual art, music and critical thinking, we cover a diverse range of experimental and emerging practices that resist classification. We promote conversation across multiple grassroot networks, paying close attention to the discursive changes brought on by developments in technology and communication. We regularly look to the periphery to present some of the most radical voices today.", email: "caroline.h@aqnb.com", team: "Editor: Steph Kretowicz (aka Jean Kay); Assistant Editor: Human Poney (aka HP Parmley); Managing Editors: Caroline Heron and Don Tercio (aka Guillermo Fraile)", genres: "Art & Design, Science & Technology, Culture & Lifestyle, Creative Writing & Poetry, Society & Capitalism", tags: "Visual Art, Music, Critique, Experimental, Grassroots, Radical, Interviews, Reviews, Creative Writing")
-Publication.create(title: "El Champ", tagline: "A queer and gay health, fitness, wellness and personal development online magazine", url: "http://elchamp.com/", cover_image_url: "https://pbs.twimg.com/profile_images/875199650839527427/FlhRm4k1_400x400.jpg", description: "Curated on a philosophy of mindful innovation and inspirational virtuosity, El Champ is the role model for the queer youth of tomorrow. A platform manifested to fill the void of a health and fitness magazine for the queer collective and to fuel the desire of personal development, wellbeing and cultural curiosity for our motivated readers. El Champ strives to exhibit dynamic and authentic resources to inspire the youth of our digital age; equipping a generation to enrich their lives, explore and engage with the queer collective.", email: "hello@elchamp.com", team: "null", genres: "Gender & Sexuality, Culture & Lifestyle", tags: "Queer, Health, Fitness, Gay, Nutrition, Exercise, Body")
-Publication.create(title: "Real Life Magazine", tagline: "null", url: "http://www.reallifemag.com", cover_image_url: "https://pbs.twimg.com/profile_images/742447324559986688/gap13Ud7_400x400.jpg", description: "Real Life is a magazine about living with technology. The emphasis is more on living. We publish one essay, advice column, reported feature, or uncategorizable piece of writing a day, four or five days a week.", email: "info@reallifemag.com", team: "Chief Editor: Nathan Jurgenson; Editors: Rob Horning, David Banks, Alexandra Molotkow, and Soraya King", genres: "Culture & Lifestyle, Society & Capitalism, Science & Technology, Creative Writing & Poetry", tags: "Media Theory, Internet, Critique, Social Media, Future")
-Publication.create(title: "Arachne", tagline: "entanglements. intersections. ontologies. gender. labor. techno.", url: "http://www.arachne.cc", cover_image_url: "https://pbs.twimg.com/profile_images/698442034852589569/rhlJLs_y_400x400.png", description: "The Arachne webzine is the result of homespun design work, which engages with each contribution on a one-to-one basis. Aspects of thought that we continue to engage with include: contemporary visual culture; intersectional gender, race, and labour studies; queer ontologies and metaphysics; human-machine interaction; trans and posthumanism; speculation; sound; magic; migration; and orientation.", email: "info@arachne.cc", team: "Dorothy Howard and André Fincato", genres: "Art & Design, Gender & Sexuality, Science & Technology, Society & Capitalism, Creative Writing & Poetry", tags: "Intersectional, gender, race, labor, queer, ontology, metaphysics, machines, transhumanism, posthumanism, speculation, sound, magic, migration, orientation")
-Publication.create(title: "The Aphrodisiac Kitchen", tagline: "null", url: "http://www.theaphrodisiackitchen.com", cover_image_url: "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/32130278_1234957773301940_2431769049445695488_n.jpg?", description: "A queer journal that examines plant based food as a cultural centerpiece through vignettes of romance, community, friendship, and more.", email: "aphrodisiackitchen@gmail.com", team: "Andreas Joshua", genres: "Culture & Lifestyle, Society & Capitalism, Gender & Sexuality", tags: "Food, Vegan, Vegetarian, Cooking, Queer, Social Justice, Food Justice")
+Publication.create(
+  title: "Topical Cream",
+  tagline: "Supporting women and gender-non-confirming individuals in contemporary art",
+  url: "http://www.topicalcream.info/",
+  cover_image_url: "https://i.etsystatic.com/isla/26d5fb/30632791/isla_280x280.30632791_2vwx0fk2.jpg",
+  description: "Topical Cream is a 501(c)3 non-profit covering women, femmes, and gender-nonconforming individuals in contemporary art. Since 2013, the New York-based platform has supported a community of artists, writers, designers, and technologists through digital publishing and public programming initiatives.",
+  email: "rx@topicalcream.info",
+  team: "Lyndsy Welgos - Co-Founder, Ara Anjargolian - Co-Founder; Whitney Mallett - Editor",
+  genres: "Culture & Lifestyle, Art & Design, Gender & Sexuality, Science & Technology",
+  tags: "Femme, Fashion, Beauty, Women, Queer, GNC, Art, Contemporary Art, Writing"
+)
+
+Publication.create(
+  title: "Arachne",
+  tagline: "entanglements. intersections. ontologies. gender. labor. techno.",
+  url: "https://www.arachne.cc",
+  cover_image_url: "https://pbs.twimg.com/profile_images/698442034852589569/rhlJLs_y_400x400.png",
+  description: "The Arachne webzine is the result of homespun design work, which engages with each contribution on a one-to-one basis. Aspects of thought that we continue to engage with include: contemporary visual culture; intersectional gender, race, and labour studies; queer ontologies and metaphysics; human-machine interaction; trans and posthumanism; speculation; sound; magic; migration; and orientation.",
+  email: "info@arachne.cc",
+  team: "Dorothy Howard - Co-Founder, André Fincato - Co-Founder",
+  genres: "Art & Design, Gender & Sexuality, Science & Technology, Society & Capitalism, Creative Writing & Poetry",
+  tags: "Intersectional, gender, race, labor, queer, ontology, metaphysics, machines, transhumanism, posthumanism, speculation, sound, magic, migration, orientation"
+)
+
+Publication.create(
+  title: "El Champ",
+  tagline: "A queer and gay health, fitness, wellness and personal development online magazine",
+  url: "https://elchamp.com/",
+  cover_image_url: "https://pbs.twimg.com/profile_images/875199650839527427/FlhRm4k1_400x400.jpg",
+  description: "Curated on a philosophy of mindful innovation and inspirational virtuosity, El Champ is the role model for the queer youth of tomorrow. A platform manifested to fill the void of a health and fitness magazine for the queer collective and to fuel the desire of personal development, wellbeing and cultural curiosity for our motivated readers. El Champ strives to exhibit dynamic and authentic resources to inspire the youth of our digital age; equipping a generation to enrich their lives, explore and engage with the queer collective.",
+  email: "hello@elchamp.com",
+  team: "null",
+  genres: "Gender & Sexuality, Culture & Lifestyle",
+  tags: "Queer, Health, Fitness, Gay, Nutrition, Exercise, Body"
+
+  Publication.create(
+    title: "The Aphrodisiac Kitchen",
+    tagline: "null",
+    url: "http://www.theaphrodisiackitchen.com",
+    cover_image_url: "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/32130278_1234957773301940_2431769049445695488_n.jpg?",
+    description: "A queer journal that examines plant based food as a cultural centerpiece through vignettes of romance, community, friendship, and more.",
+    email: "aphrodisiackitchen@gmail.com",
+    team: "Andreas Joshua - Founder",
+    genres: "Culture & Lifestyle, Society & Capitalism, Gender & Sexuality",
+    tags: "Food, Vegan, Vegetarian, Cooking, Queer, Social Justice, Food Justice"
+  )
+)
+
+Publication.create(
+  title: "Real Life Magazine",
+  tagline: "null",
+  url: "http://www.reallifemag.com",
+  cover_image_url: "https://pbs.twimg.com/profile_images/742447324559986688/gap13Ud7_400x400.jpg",
+  description: "Real Life is a magazine about living with technology. The emphasis is more on living. We publish one essay, advice column, reported feature, or uncategorizable piece of writing a day, four or five days a week.",
+  email: "info@reallifemag.com",
+  team: "Nathan Jurgenson - Chief Editor, Rob Horning - Editor, David Banks - Editor, Alexandra Molotkow - Editor, Soraya King - Editor",
+  genres: "Culture & Lifestyle, Society & Capitalism, Science & Technology, Creative Writing & Poetry",
+  tags: "Media Theory, Internet, Critique, Social Media, Future"
+)
+
+
+Publication.create(
+  title: "Unbag Magazine",
+  tagline: "An arts, culture, and politics publication",
+  url: "http://www.unbag.net" ,
+  cover_image_url: "http://www.brooklynartscouncil.org/files/images/31047_Unbag_Square_Icon_Blue-2_1500901207.jpg",
+  description: "Unbag is a semi-annual magazine that promotes critical engagement with contemporary art and politics. Commissioning artists, writers, and thinkers who work outside of mainstream discourses, unbag functions as a space to explore ideas through discussion and exchange.",
+  email: "info@unbag.net",
+  team: "Aaron Cooper - Editor, Charlie Markbreiter - Editor, Natalia Tuero - Editor, Andy Wentz - Operations and Production, American Artist - Art Direction and Print Design, Logan Lape - Web Design and Web Development",
+  genres: "Art & Design, Culture & Lifestyle, Society & Capitalism",
+  tags: "New York City, Art, Contemporary Art, Politics, Writing, Critique")
+
+
+Publication.create(
+  title: "Mask Magazine",
+  tagline: "Culture and style for misfits and malcontents",
+  url: "http://www.maskmagazine.com",
+  cover_image_url: "http://s3.amazonaws.com/issue-assets/authors/images/000/000/131/original/10340159_516349021821649_3881028931068449400_n.png",
+  description: "Mask Magazine is an online repository of youth and internet culture packed with interviews, editorials, news, and style in the age of unrest. Mask is member-supported and advertisement free.",
+  email: "mailroom@mask-mag.com",
+  team: "Hannah Hurr - Chief Editor, Tyler Reinhard - Co-Founder, Ripley Soprano - Co-Founder",
+  genres: "Culture & Lifestyle, Society & Capitalism",
+  tags: "New York City, Underground, News, Reviews, Editorials, Interviews, Style, End times"
+)
+
+
+Publication.create(
+  title: "AQNB",
+  tagline: "null",
+  url: "https://www.aqnb.com",
+  cover_image_url: "https://d23pzp3qb0c2ie.cloudfront.net/2018/03/cropped-AQNB-favicon-square-logo.png",
+  description: "AQNB is a not-for-profit editorial platform committed to independent media. Working at the intersection of visual art, music and critical thinking, we cover a diverse range of experimental and emerging practices that resist classification. We promote conversation across multiple grassroot networks, paying close attention to the discursive changes brought on by developments in technology and communication. We regularly look to the periphery to present some of the most radical voices today.",
+  email: "caroline.h@aqnb.com",
+  team: "Steph Kretowicz (aka Jean Kay) - Editor, HP Parmley - Assistant Editor, Caroline Heron - Managing Editor, Don Tercio (aka Guillermo Fraile) - Managing Editor",
+  genres: "Art & Design, Science & Technology, Culture & Lifestyle, Creative Writing & Poetry, Society & Capitalism",
+  tags: "Visual Art, Music, Critique, Experimental, Grassroots, Radical, Interviews, Reviews, Creative Writing"
+)
+
+Publication.create(
+  title: "Hazlit",
+  tagline: "All the best stories: culture, reporting, interviews, podcasts, comics and fiction from Hazlitt.",
+  url: "https://hazlitt.net/",
+  cover_image_url: "https://pbs.twimg.com/profile_images/2515463167/83dia5mtj1eef2ue8o9r_400x400.jpeg",
+  description: "Hazlitt is a home for writers and artists to tell the best stories about the things that matter most to them. Be it art, sound, or text, fiction or non-fiction, humour or criticism, Hazlitt is essential Internet: humane, diverse, and committed to stories and writers not heard anywhere else."
+  email: "null",
+  team: "null",
+  genres: "Creative Writing & Poetry, Art & Design"
+  tags: "Fiction, Non-Fiction, Stories, Interviews, Contemporary"
+)
+
+Publication.create(
+  title: "Into The Gloss",
+  tagline: "A beauty experience—interviews, inspiration, and the products you need to know about.",
+  url: "https://intothegloss.com/"
+  cover_image_url: "https://pbs.twimg.com/profile_images/876919650419961858/CGxVc5P0_400x400.jpg",
+  description: "Into The Gloss is a beauty website that explores the routines of inspiring women. Founded by Emily Weiss in 2010, the site is known for its profiles, particularly The Top Shelf and Top Shelf After Dark. ITG editors sit on the bathroom floors of everyone from Jenna Lyons to Kim Kardashian to talk products, career, and what beauty means to women today. They review the products they love and the products you need to know about.",
+  email: "editorial.info@glossier.com",
+  team: "Emily Ferber - Editorial Director, Tom Newton - Senior Visuals Editor, Anna Stevenett - Editorial Assistant, Utibe Mbagqu - Content Coordinator",
+  genres: "Culture & Lifestyle",
+  tags: "Beauty, Skincare, POC, Femme, Women"
+)
