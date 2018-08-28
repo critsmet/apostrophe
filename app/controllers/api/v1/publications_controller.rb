7067c49f61de0ee339838c35175fab70d8b71daf
critsmet <36425627+critsmet@users.noreply.github.com>
2018-08-28 00:05:59 -0400

    Lower

--- app/controllers/api/v1/publications_controller.rb
+++ app/controllers/api/v1/publications_controller.rb
@@ -4,8 +4,8 @@ module Api
 
 			def search
 				if params["terms"][1] == '' || params["terms"][1] == "all"
-					@search = params["terms"][0].titleize
-					@publications = Publication.where("tags like ?", "%#{@search}%").or( Publication.where("title like ?", "%#{@search}%") )
+					@search = params["terms"][0].downcase
+					@publications = Publication.where("lower(tags) like ?", "%#{@search}%").or( Publication.where("lower(title) like ?", "%#{@search}%") )
 					render json: @publications
 				elsif params["terms"][0] == ''
 					if params["terms"][1] == 'all'
@@ -17,10 +17,10 @@ module Api
 						render json: @publications
 					end
 				else
-					@search = params["terms"][0].capitalize
+					@search = params["terms"][0].downcase
 					@filter = params["terms"][1].split('-').map(&:capitalize).join(' ')
 					@filtered_pubs = @publications = Publication.where("genres like ?", "%#{@filter}%")
-					@searched_filtered = @filtered_pubs.where("tags like ?", "%#{@search}%").or( Publication.where("title like ?", "%#{@search}%") )
+					@searched_filtered = @filtered_pubs.where("lower(tags) like ?", "%#{@search}%").or( Publication.where("lower(title) like ?", "%#{@search}%") )
 					render json: @searched_filtered
 				end
 			end
