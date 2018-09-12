e6089ffcdc0a5bcb7e6a88ad4660deffab30b24b
critsmet <36425627+critsmet@users.noreply.github.com>
2018-09-12 15:04:22 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,6 @@ Publication.create(
   url: "http://www.topicalcream.info/",
   cover_image_url: "https://i.etsystatic.com/isla/26d5fb/30632791/isla_280x280.30632791_2vwx0fk2.jpg",
   description: "Topical Cream is a 501(c)3 non-profit covering women, femmes, and gender-non-conforming individuals in contemporary art. Since 2013, the New York-based platform has supported a community of artists, writers, designers, and technologists through digital publishing and public programming initiatives.",
-  email: "rx@topicalcream.info",
   team: "Lyndsy Welgos - Co-Founder, Ara Anjargolian - Co-Founder; Whitney Mallett - Editor",
   genres: "Culture & Lifestyle, Art & Design, Gender & Sexuality, Science & Technology",
   tags: "Femme, Fashion, Beauty, Women, Queer, GNC, Art, Contemporary Art, Writing"
@@ -24,7 +23,6 @@ Publication.create(
   url: "https://www.arachne.cc",
   cover_image_url: "https://pbs.twimg.com/profile_images/698442034852589569/rhlJLs_y_400x400.png",
   description: "The Arachne webzine is the result of homespun design work, which engages with each contribution on a one-to-one basis. Aspects of thought that we continue to engage with include: contemporary visual culture; intersectional gender, race, and labour studies; queer ontologies and metaphysics; human-machine interaction; trans and posthumanism; speculation; sound; magic; migration; and orientation.",
-  email: "info@arachne.cc",
   team: "Dorothy Howard - Co-Founder, André Fincato - Co-Founder",
   genres: "Art & Design, Gender & Sexuality, Science & Technology, Society & Capitalism, Creative Writing & Poetry",
   tags: "Intersectional, gender, race, labor, queer, ontology, metaphysics, machines, transhumanism, posthumanism, speculation, sound, magic, migration, orientation"
@@ -36,7 +34,6 @@ Publication.create(
   url: "https://elchamp.com/",
   cover_image_url: "https://pbs.twimg.com/profile_images/875199650839527427/FlhRm4k1_400x400.jpg",
   description: "Curated on a philosophy of mindful innovation and inspirational virtuosity, El Champ is a platform manifested to fill the void of a health and fitness magazine for the queer collective.",
-  email: "hello@elchamp.com",
   team: "null",
   genres: "Gender & Sexuality, Culture & Lifestyle",
   tags: "Queer, Health, Fitness, Gay, Nutrition, Exercise, Body"
@@ -48,7 +45,6 @@ Publication.create(
     url: "http://www.theaphrodisiackitchen.com",
     cover_image_url: "https://scontent-lga3-1.cdninstagram.com/vp/5f122868f2fc551831570c5d51c3ebe7/5BFFF407/t51.2885-15/e35/32832882_435181350243646_1921239214821736448_n.jpg",
     description: "A queer journal that examines plant based food as a cultural centerpiece through vignettes of romance, community, friendship, and more",
-    email: "aphrodisiackitchen@gmail.com",
     team: "Andreas Joshua - Founder",
     genres: "Culture & Lifestyle, Society & Capitalism, Gender & Sexuality",
     tags: "Food, Vegan, Vegetarian, Cooking, Queer, Social Justice, Food Justice"
@@ -60,7 +56,6 @@ Publication.create(
   url: "https://intothegloss.com/",
   cover_image_url: "https://pbs.twimg.com/profile_images/876919650419961858/CGxVc5P0_400x400.jpg",
   description: "Into The Gloss is a beauty website that explores the routines of inspiring women, founded by Emily Weiss in 2010. ITG editors sit on the bathroom floors of everyone from Jenna Lyons to Kim Kardashian to talk products, career, and what beauty means to women today.",
-  email: "editorial.info@glossier.com",
   team: "Emily Ferber - Editorial Director, Tom Newton - Senior Visuals Editor, Anna Stevenett - Editorial Assistant, Utibe Mbagqu - Content Coordinator",
   genres: "Culture & Lifestyle",
   tags: "Beauty, Skincare, POC, Femme, Women"
@@ -72,7 +67,6 @@ Publication.create(
   url: "http://www.unbag.net" ,
   cover_image_url: "http://www.brooklynartscouncil.org/files/images/31047_Unbag_Square_Icon_Blue-2_1500901207.jpg",
   description: "Unbag is a semi-annual magazine that promotes critical engagement with contemporary art and politics. Commissioning artists, writers, and thinkers who work outside of mainstream discourses, unbag functions as a space to explore ideas through discussion and exchange.",
-  email: "info@unbag.net",
   team: "Aaron Cooper - Editor, Charlie Markbreiter - Editor, Natalia Tuero - Editor, Andy Wentz - Operations and Production, American Artist - Art Direction and Print Design, Logan Lape - Web Design and Web Development",
   genres: "Art & Design, Culture & Lifestyle, Society & Capitalism",
   tags: "New York City, Art, Contemporary Art, Politics, Writing, Critique"
@@ -84,7 +78,6 @@ Publication.create(
   url: "http://www.maskmagazine.com",
   cover_image_url: "http://s3.amazonaws.com/issue-assets/authors/images/000/000/131/original/10340159_516349021821649_3881028931068449400_n.png",
   description: "Mask Magazine is an online repository of youth and internet culture packed with interviews, editorials, news, and style in the age of unrest. Mask is member-supported and advertisement free.",
-  email: "mailroom@mask-mag.com",
   team: "Hannah Hurr - Chief Editor, Tyler Reinhard - Co-Founder, Ripley Soprano - Co-Founder",
   genres: "Culture & Lifestyle, Society & Capitalism",
   tags: "New York City, Underground, News, Reviews, Editorials, Interviews, Style, End times"
@@ -96,7 +89,6 @@ Publication.create(
   url: "https://www.aqnb.com",
   cover_image_url: "https://d23pzp3qb0c2ie.cloudfront.net/2018/03/cropped-AQNB-favicon-square-logo.png",
   description: "Working at the intersection of visual art, music and critical thinking, AQNB covers a diverse range of experimental and emerging practices that resist classification.",
-  email: "caroline.h@aqnb.com",
   team: "Steph Kretowicz (aka Jean Kay) - Editor, HP Parmley - Assistant Editor, Caroline Heron - Managing Editor, Don Tercio (aka Guillermo Fraile) - Managing Editor",
   genres: "Art & Design, Science & Technology, Culture & Lifestyle, Creative Writing & Poetry, Society & Capitalism",
   tags: "Visual Art, Music, Critique, Experimental, Grassroots, Radical, Interviews, Reviews, Creative Writing"
@@ -108,7 +100,6 @@ Publication.create(
   url: "https://hazlitt.net/",
   cover_image_url: "https://pbs.twimg.com/profile_images/2515463167/83dia5mtj1eef2ue8o9r_400x400.jpeg",
   description: "Hazlitt is a home for writers and artists to tell the best stories about the things that matter most to them. Be it art, sound, or text, fiction or non-fiction, humour or criticism, Hazlitt is essential Internet: humane, diverse, and committed to stories and writers not heard anywhere else.",
-  email: "null",
   team: "null",
   genres: "Creative Writing & Poetry, Art & Design",
   tags: "Fiction, Non-Fiction, Stories, Interviews, Contemporary, Writing"
@@ -120,7 +111,6 @@ Publication.create(
   url: "http://www.reallifemag.com",
   cover_image_url: "https://pbs.twimg.com/profile_images/742447324559986688/gap13Ud7_400x400.jpg",
   description: "Real Life is a magazine about living with technology. The emphasis is more on living. We publish one essay, advice column, reported feature, or uncategorizable piece of writing a day, four or five days a week.",
-  email: "info@reallifemag.com",
   team: "Nathan Jurgenson - Chief Editor, Rob Horning - Editor, David Banks - Editor, Alexandra Molotkow - Editor, Soraya King - Editor",
   genres: "Culture & Lifestyle, Society & Capitalism, Science & Technology, Creative Writing & Poetry",
   tags: "Media Theory, Internet, Critique, Social Media, Future"
@@ -132,7 +122,6 @@ Publication.create(
   url: "https://www.counterservicemag.com/",
   cover_image_url: "https://pbs.twimg.com/profile_images/831233852907585536/GqwqwmHY_400x400.jpg",
   description: "Counter Service is a publication that looks at the intentional & unintentional ways food impacts and influences our lives. We focus on those who are enriching the culture around food, and who are often not highlighted in the ever fast-paced, trend-aware food world.",
-  email: "hello@counterservicemag.com",
   team: "null",
   genres: "Culture & Lifestyle",
   tags: "Food, Cooking, Culture"
@@ -144,7 +133,6 @@ Publication.create(
   url: "https://www.coeval-magazine.com/",
   cover_image_url: "https://scontent-lga3-1.cdninstagram.com/vp/bef632aa8e32c35ca5b5afda617e3c32/5C3B84E0/t51.2885-19/s320x320/28434839_151770135499860_8073466690211414016_n.jpg",
   description: "Inspiring source for the conscious rebel, Coeval, the encyclopaedic-format publication, explores the new era artists, musicians designers, photographers, directors and people by the avant-garde perspective and today’s vibrant subcultures.",
-  email: "contact@coeval-magazine.com",
   team: "null",
   genres: "Art & Design, Culture & Lifestyle",
   tags: "Music, Art, Fashion, Design, Photography, Experimental, Interviews, Contemporary, Culture"
@@ -156,7 +144,6 @@ Publication.create(
   url: "https://restforresistance.com/",
   cover_image_url: "https://pbs.twimg.com/profile_images/981609579908870150/juJO4FbN_400x400.jpg",
   description: "Rest for Resistance strives to uplift marginalized communities, those who rarely get access to adequate health care or social support. This includes Black, Indigenous, Latinx, Pacific Islander, Asian, Middle Eastern, and multiracial persons.",
-  email: "hello@restforresistance.com",
   team: "Dom Chatterjee - Editor, Eve Moreno - Editor, Princess Harmony Rodriguez - Editor, Venus Di'Khadijah Selenite - Editor, Domin Bradley - Editor, O.A.O. - Editor, Kofi Opam - Editor",
   genres: "Culture & Lifestyle, Creative Writing & Poetry, Gender & Sexuality, Society & Capitalism",
   tags: "Mental Health, QTPOC, POC, Queer, Resistance, Poetry, Creative Writing"
@@ -168,7 +155,6 @@ Publication.create(
   url: "https://www.pw-magazine.com/",
   cover_image_url: "https://pbs.twimg.com/profile_images/1013420997658271749/Sb_dYXGR_400x400.jpg",
   description: "PW-Magazine is a Vienna-based online magazine for contemporary culture. By giving voice to a wide array of cutting-edge personas in art and culture, the magazine promotes diversity and a broad mix of artistic expression.",
-  email: "editorial@pw-magazine.com",
   team: "Christian Glatz, Phil Koch, Marie-Claire Gagnon, Christian Glatz, Ada Karlbauer, Amar Priganica, Julius Pristauz, Laura Schaeffer, Hannah Christ, Elisabeth Falkensteiner, Wera Hippesroither, Juliana Lindenhofer, Pia-Marie Remmers, Alexandra-Maria Toth",
   genres: "Culture & Lifestyile, Art & Design",
   tags: "Contemporary, Culture, Art, Visual Art, Design"
